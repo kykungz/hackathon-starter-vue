8c96cc9c5dc5d13675a713a45512b0bf1e74dde5
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-03-12 16:53:07 +0700

    :sparkles: Add Template

--- src/components/DemoTemplate.vue
+++ src/components/DemoTemplate.vue
@@ -1,23 +1,14 @@
 <template>
-  <div id="home">
-    <h1 class="text-center">
-      <i class="fa fa-home" aria-hidden="true"></i>
-      Home
-    </h1>
-    <demo-vuex></demo-vuex>
+  <div id="">
   </div>
 </template>
 
 <script>
-import DemoVuex from './DemoVuex.vue'
-
 export default {
   props: [],
-  components: { DemoVuex },
+  components: {},
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+    return {}
   },
   methods: {},
   computed: {},
