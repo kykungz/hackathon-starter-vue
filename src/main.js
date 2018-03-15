af5d1023726cebd11450912cae522e89879bbf27
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-03-15 23:46:03 +0700

    :recycle: Fix spacing

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import ShareTwitter from './global/ShareTwitter'
 import SocialSharing from 'vue-social-sharing'
 import BootstrapVue from 'bootstrap-vue'
 
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'font-awesome/css/font-awesome.css'
